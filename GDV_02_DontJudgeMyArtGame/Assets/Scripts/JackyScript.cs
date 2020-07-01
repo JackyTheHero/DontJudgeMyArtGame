9dcd7130b22a272c2c603dc59c84b9a79a210fe3
JackyTheHero <held.jacqueline@gmx.de>
2020-07-01 17:05:53 +0200

    new object, JackyScript edited
    - added Empty visitorScriptTest to Scene
    - connected vistitorScriptTest with JackyScript
    - edited JackyScript for first visitor/text bubble test

--- GDV_02_DontJudgeMyArtGame/Assets/Scripts/JackyScript.cs
+++ GDV_02_DontJudgeMyArtGame/Assets/Scripts/JackyScript.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class JackyScript : MonoBehaviour
 {
+
+    GameObject visitor; // visitor height --> 4 tiles
+    GameObject textBubble;
+
     // Start is called before the first frame update
     void Start()
     {
+        visitor = GameObject.Find("figureWIP1");
+        textBubble = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        textBubble.active = false;
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (visitor.transform.position.x < 3.0f)
+        {
+            visitor.transform.position += new Vector3(0.01f, 0.0f, 0.01f);
+            textBubble.transform.position = visitor.transform.position + new Vector3(0.0f, 2f, 0.0f);
+        }
+        else {
+            textBubble.active = true;
+            if (textBubble.transform.position.y < 5.0f)
+            {
+                textBubble.transform.position += new Vector3(0.0f, 0.01f, 0.0f);
+            }
+        }
+        textBubble.transform.Rotate(0.0f,0.1f,0.0f);
+        //Debug.Log("X-Position:" + visitor.transform.position.x);
     }
 }
