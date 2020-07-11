953bef1caf89124719004a6abb50e63a69994861
Melanie Werner <64008638+Anlariel@users.noreply.github.com>
2020-07-11 19:43:10 +0200

    Player movement
    - changed some values in order to have a slower player movement
    - made mouse cursor invisible

--- GDV_02_DontJudgeMyArtGame/Assets/Scripts/MW_playerScript.cs
+++ GDV_02_DontJudgeMyArtGame/Assets/Scripts/MW_playerScript.cs
@@ -9,10 +9,14 @@ public class MW_playerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Maus-Cursor wird ausgeblendet
+        Cursor.visible = false;
+        
         // Kamera folgt player
         Camera.main.transform.position = player.transform.position;
         Camera.main.transform.parent = player.transform;
-        Camera.main.transform.Translate(0, 2, -6);
+        Camera.main.transform.Translate(0, 3.8f, -6.0f);
+        Camera.main.transform.Rotate(8.0f, 0, 0);
     }
 
     // Quaternion lastRotation;
@@ -26,28 +30,28 @@ public class MW_playerScript : MonoBehaviour
 
         // Mausbewegung nach links
         if(Input.GetAxis("Mouse X") < 0) {
-            player.transform.rotation *= Quaternion.AngleAxis(-5.0f, Vector3.up);
+            player.transform.rotation *= Quaternion.AngleAxis(-2.0f, Vector3.up);
         }
         // Mausbewegung nach rechts
         if(Input.GetAxis("Mouse X") > 0) {
-            player.transform.rotation *= Quaternion.AngleAxis(5.0f, Vector3.up);
+            player.transform.rotation *= Quaternion.AngleAxis(2.0f, Vector3.up);
         }
 
         // GetKey läuft kontinuierlich in jeweilige Richtung, solange Button gedrückt wird
         // GetKeyDown führt Aktion bei einem Tastendruck jeweils nur einmal aus
         if (Input.GetKey(KeyCode.W)) {
-            player.transform.position += player.transform.localRotation * new Vector3(0, 0, -1.0f);
+            player.transform.position += player.transform.localRotation * new Vector3(0, 0, -0.1f);
         }
         if (Input.GetKey(KeyCode.A)) {
-            player.transform.position += player.transform.localRotation * new Vector3(1.0f, 0, 0);
+            player.transform.position += player.transform.localRotation * new Vector3(0.1f, 0, 0);
             // Drehung ohne Maus um -90 Grad:
             // player.transform.rotation *= Quaternion.AngleAxis(-90.0f, Vector3.up);
         }
         if (Input.GetKey(KeyCode.S)) {
-            player.transform.position += player.transform.localRotation * new Vector3(0, 0, 1.0f);
+            player.transform.position += player.transform.localRotation * new Vector3(0, 0, 0.1f);
         }
         if (Input.GetKey(KeyCode.D)) {
-            player.transform.position += player.transform.localRotation * new Vector3(-1.0f, 0, 0);
+            player.transform.position += player.transform.localRotation * new Vector3(-0.1f, 0, 0);
             // Drehung ohne Maus um 90 Grad:
             // player.transform.rotation *= Quaternion.AngleAxis(90.0f, Vector3.up);
         }
