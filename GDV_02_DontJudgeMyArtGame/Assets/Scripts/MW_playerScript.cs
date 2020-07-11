de8bc91c6f64ee9eb051269ab806aa852f909f96
Melanie Werner <64008638+Anlariel@users.noreply.github.com>
2020-07-11 14:05:20 +0200

    Fundamental player movement
    - player can move the character by turning the mouse to the left or right
    - player can then head into a specific direction by pressing W, A, S or D
    - movement is too fast in the current state which makes it hard to head to a specific spot

--- GDV_02_DontJudgeMyArtGame/Assets/Scripts/MW_playerScript.cs
+++ GDV_02_DontJudgeMyArtGame/Assets/Scripts/MW_playerScript.cs
@@ -15,9 +15,42 @@ public class MW_playerScript : MonoBehaviour
         Camera.main.transform.Translate(0, 2, -6);
     }
 
+    // Quaternion lastRotation;
+    // Quaternion targetRotation;
+
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+        // lastRotation = player.transform.rotation;
+        // float moveTime = 1.5f;
+        // player.transform.rotation = Quaternion.Lerp(lastRotation, targetRotation, Time.time * moveTime);
+
+        // Mausbewegung nach links
+        if(Input.GetAxis("Mouse X") < 0) {
+            player.transform.rotation *= Quaternion.AngleAxis(-5.0f, Vector3.up);
+        }
+        // Mausbewegung nach rechts
+        if(Input.GetAxis("Mouse X") > 0) {
+            player.transform.rotation *= Quaternion.AngleAxis(5.0f, Vector3.up);
+        }
+
+        // GetKey läuft kontinuierlich in jeweilige Richtung, solange Button gedrückt wird
+        // GetKeyDown führt Aktion bei einem Tastendruck jeweils nur einmal aus
+        if (Input.GetKey(KeyCode.W)) {
+            player.transform.position += player.transform.localRotation * new Vector3(0, 0, -1.0f);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            player.transform.position += player.transform.localRotation * new Vector3(1.0f, 0, 0);
+            // Drehung ohne Maus um -90 Grad:
+            // player.transform.rotation *= Quaternion.AngleAxis(-90.0f, Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            player.transform.position += player.transform.localRotation * new Vector3(0, 0, 1.0f);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            player.transform.position += player.transform.localRotation * new Vector3(-1.0f, 0, 0);
+            // Drehung ohne Maus um 90 Grad:
+            // player.transform.rotation *= Quaternion.AngleAxis(90.0f, Vector3.up);
+        }
     }
+     
 }
