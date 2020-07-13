70975e5eafb73557cdaef39a9dd848f356d73041
Daiuuus <63926891+Daiuuus@users.noreply.github.com>
2020-07-13 16:48:32 +0200

    Rahmen + Texturen

--- GDV_02_DontJudgeMyArtGame/Assets/Scripts/Darius_BildScript_01.cs
+++ GDV_02_DontJudgeMyArtGame/Assets/Scripts/Darius_BildScript_01.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Darius_BildScript_01 : MonoBehaviour
 {
-    // Start is called before the first frame update
+// Start is called before the first frame update
     void Start()
     {
-        createBild(0f, 0f, 6, 5);  
+        createBild(0f, 0f, 6f, 4f, 2f);  
         
     }
 
@@ -17,7 +17,11 @@ public class Darius_BildScript_01 : MonoBehaviour
         
     }
 
-    void createBild(float posX, float posZ, int sizeX, int sizeY){
+    void createBild(float posX, float posZ, float sizeX, float sizeY, float height){
+
+        float thick = 0.3f;
+        float depth = 0.3f;
+        float abstand = 0.2f;
 
         //GameObject erzeugen
         GameObject bild = new GameObject();
@@ -29,26 +33,61 @@ public class Darius_BildScript_01 : MonoBehaviour
         Mesh bildflaeche = bild.GetComponent<MeshFilter>().mesh;
         
         //Arrays für Bildfläche
-        bildflaeche.vertices = new Vector3[] {new Vector3(0 + posX, 2, 0 + posZ), new Vector3(sizeX + posX, 2, 0 + posZ), new Vector3(sizeX + posX, sizeY, 0 + posZ), new Vector3(0 + posX, sizeY, 0 + posZ)};
+        bildflaeche.vertices = new Vector3[] {new Vector3((0 - sizeX / 2) + posX, height, posZ - abstand), new Vector3(sizeX / 2 + posX, height, posZ - abstand), new Vector3(sizeX / 2 + posX, sizeY + height, posZ - abstand), new Vector3((0 - sizeX / 2) + posX, sizeY + height, posZ - abstand)};
         bildflaeche.triangles = new int[] {3, 2, 1, 3, 1, 0};
 
-        //Berechne uvs
-        Vector3[] vertices = bildflaeche.vertices;
-        Vector2[] uvs = new Vector2[vertices.Length];
-
-        for (int i = 0; i < uvs.Length; i++)
-        {
-            uvs[i] = new Vector2(vertices[i].x, vertices[i].z);
-        }
-        bildflaeche.uv = uvs;
+        bildflaeche.uv = new Vector2[] {new Vector2(0,0), new Vector2(1,0), new Vector2(1,1), new Vector2(0,1)};
 
         bildflaeche.RecalculateBounds();
 
         //Renderer
-        Renderer rend = bild.GetComponent<Renderer>();
-        rend.material= new Material(Shader.Find("Standard"));
-        rend.material.color = Color.blue;
+        Renderer rendB = bild.GetComponent<Renderer>();
+        rendB.material= new Material(Shader.Find("Standard"));
+        rendB.material.mainTexture = Resources.Load("testbild") as Texture;
 
         bildflaeche.RecalculateNormals();
+
+        //GameObject erzeugen
+        GameObject rahmen = new GameObject();
+
+        rahmen.AddComponent<MeshFilter>();
+        rahmen.AddComponent<MeshRenderer>();
+
+        //Mesh erzeugen
+        Mesh rahmenflaeche = rahmen.GetComponent<MeshFilter>().mesh;
+        
+        
+        //Arrays für Bildfläche
+        rahmenflaeche.vertices = new Vector3[] {
+            new Vector3((0 - sizeX / 2) + posX - thick, height - thick, posZ - depth), 
+            new Vector3(sizeX / 2 + posX + thick, height - thick, posZ - depth), 
+            new Vector3(sizeX / 2 + posX + thick, sizeY + height + thick, posZ - depth), 
+            new Vector3((0 - sizeX / 2) + posX - thick, sizeY + height + thick, posZ - depth),
+            new Vector3((0 - sizeX / 2) + posX - thick, height - thick, posZ), 
+            new Vector3(sizeX / 2 + posX + thick, height - thick, posZ), 
+            new Vector3(sizeX / 2 + posX + thick, sizeY + height + thick, posZ), 
+            new Vector3((0 - sizeX / 2) + posX - thick, sizeY + height + thick, posZ),
+            new Vector3((0 - sizeX / 2) + posX, height, posZ - depth),
+            new Vector3(sizeX / 2 + posX, height, posZ - depth), 
+            new Vector3(sizeX / 2 + posX, sizeY + height, posZ - depth), 
+            new Vector3((0 - sizeX / 2) + posX, sizeY + height, posZ - depth),
+            new Vector3((0 - sizeX / 2) + posX, height, posZ - abstand), 
+            new Vector3(sizeX / 2 + posX, height, posZ - abstand), 
+            new Vector3(sizeX / 2 + posX, sizeY + height, posZ - abstand), 
+            new Vector3((0 - sizeX / 2) + posX, sizeY + height, posZ - abstand)
+        };
+
+        rahmenflaeche.triangles = new int[] {0,1,5,0,5,4, 0,4,7,0,7,3, 1,2,6,1,6,5, 2,3,7,2,7,6, 0,8,9,0,9,1, 0,3,11,0,11,8, 1,9,10,1,10,2, 10,11,3,10,3,2, 8,12,13,8,13,9, 8,11,15,8,15,12, 9,13,14,9,14,10, 14,15,11,14,11,10};
+
+        rahmenflaeche.uv = new Vector2[] {new Vector2(0,0), new Vector2(1,0), new Vector2(0,0), new Vector2(1,0), new Vector2(0,1), new Vector2(1,1), new Vector2(0,1), new Vector2(1,1), new Vector2(0,1), new Vector2(1,1), new Vector2(0,1), new Vector2(1,1), new Vector2(0,0), new Vector2(1,0), new Vector2(0,0), new Vector2(1,0)};
+
+        rahmenflaeche.RecalculateBounds();
+
+        //Renderer
+        Renderer rendR = rahmen.GetComponent<Renderer>();
+        rendR.material= new Material(Shader.Find("Standard"));
+        rendR.material.mainTexture = Resources.Load("holz") as Texture;
+
+        rahmenflaeche.RecalculateNormals();
     }
 }
