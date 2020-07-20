3b935e2c83bf6b946e4953335f35e514c54054eb
Melanie Werner <64008638+Anlariel@users.noreply.github.com>
2020-07-20 16:06:10 +0200

    Free Look Camera / Line of sight
    - changed camera movement so that the camera moves around the player when turning the mouse
    - player is now looking into the moving direction
    - key W is locked when S is being pressed (and vice versa)
    - note: current problem is that one cannot press two keys at a time (e.g. W and D) withoug causing a complete chaos

--- GDV_02_DontJudgeMyArtGame/Assets/Scripts/MW_playerScript.cs
+++ GDV_02_DontJudgeMyArtGame/Assets/Scripts/MW_playerScript.cs
@@ -6,9 +6,10 @@ public class MW_playerScript : MonoBehaviour
 {
     public GameObject player;
 
-
     float speed = 0.1f;
     float slowSpeed = 0.03f;
+
+    Vector3 cameraView;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +17,17 @@ public class MW_playerScript : MonoBehaviour
         // Maus-Cursor wird ausgeblendet
         Cursor.visible = false;
         
+        // ohne freeLookCamera
         // Kamera folgt player
         Camera.main.transform.position = player.transform.position;
         Camera.main.transform.parent = player.transform;
-        Camera.main.transform.Translate(0, 8.0f, -10.0f);
-        Camera.main.transform.Rotate(15.0f, 0, 0);
+        /*Camera.main.transform.Translate(0, 8.0f, -10.0f);
+        Camera.main.transform.Rotate(15.0f, 0, 0);*/
+
+        // Setze Anfangssicht der Kamera in Relation zum Spieler
+        // da Spieler zu Beginn auf 0|0|0 steht, kann mit festen Werten gearbeitet werden, anstelle bspw. player.transform.position.x - 10.0f
+        // nur in Start() setzen, da sich cameraView in Update fortlaufend ändert
+        cameraView = new Vector3(-10.0f, 8.0f, 0);
     }
 
     // Quaternion lastRotation;
@@ -31,8 +38,12 @@ public class MW_playerScript : MonoBehaviour
         // lastRotation = player.transform.rotation;
         // player.transform.rotation = Quaternion.Lerp(lastRotation, targetRotation, Time.time * moveTime);
 
+        freeLookCamera();
+
+        // Blockiere S, wenn W gedrückt wird - und umgekehrt
+        bool keyLocked = false;
         
-        float mouseSpeed = 2.0f;
+        // float mouseSpeed = 2.0f;
 
         // slowMode wird aktiviert, wenn Shift gedrückt wird (links oder rechts)
         var slowMode = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
@@ -43,31 +54,66 @@ public class MW_playerScript : MonoBehaviour
         // player läuft also bei gedrückter Shift-Taste langsamer
         var speed = slowMode ? this.slowSpeed : this.speed;
 
+        // ohne freeLookCamera
         // Mausbewegung nach links
-        if(Input.GetAxis("Mouse X") < 0) {
+        /*if(Input.GetAxis("Mouse X") < 0) {
             player.transform.rotation *= Quaternion.AngleAxis(-mouseSpeed, Vector3.up);
         }
         // Mausbewegung nach rechts
         if(Input.GetAxis("Mouse X") > 0) {
             player.transform.rotation *= Quaternion.AngleAxis(mouseSpeed, Vector3.up);
-        }
+        }*/
 
         // GetKey läuft kontinuierlich in jeweilige Richtung, solange Button gedrückt wird
         // GetKeyDown führt Aktion bei einem Tastendruck jeweils nur einmal aus
-        if (Input.GetKey(KeyCode.W)) {
+        if (Input.GetKey(KeyCode.W) && keyLocked == false) {
+            // Setze Blickrichtung des Players auf Blickrichtung der Kamera
+            player.transform.forward = -GameObject.Find("Main Camera").transform.forward;
+            // da Kamera nicht exakt geradeaus schaut, muss Player-Position nachjustiert werden
+            player.transform.Rotate(23.66f, player.transform.position.y, 0);
             player.transform.position += player.transform.localRotation * new Vector3(0, 0, -speed);
+            keyLocked = true;
+            // player.transform.rotation = Quaternion.LookRotation(-player.transform.position);
         }
         if (Input.GetKey(KeyCode.A)) {
-            player.transform.position += player.transform.localRotation * new Vector3(speed, 0, 0);
-            // player.transform.rotation *= Quaternion.AngleAxis(-speed * 10, Vector3.up);
+            // Setze Blickrichtung des Players auf Blickrichtung der Kamera
+            player.transform.forward = GameObject.Find("Main Camera").transform.right;
+            // da Kamera nicht exakt geradeaus schaut, muss Player-Position nachjustiert werden
+            // player.transform.Rotate(23.66f, player.transform.position.y, 0);
+            player.transform.position += player.transform.localRotation * new Vector3(0, 0, -speed);
+            // player.transform.rotation = Quaternion.LookRotation(-player.transform.position);
         }
-        if (Input.GetKey(KeyCode.S)) {
-            player.transform.position += player.transform.localRotation * new Vector3(0, 0, speed);
+        if (Input.GetKey(KeyCode.S) && keyLocked == false) {
+            // Setze Blickrichtung des Players auf Blickrichtung der Kamera
+            player.transform.forward = GameObject.Find("Main Camera").transform.forward;
+            // da Kamera nicht exakt geradeaus schaut, muss Player-Position nachjustiert werden
+            player.transform.Rotate(-23.66f, player.transform.position.y, 0);
+            player.transform.position += player.transform.localRotation * new Vector3(0, 0, -speed);
+            keyLocked = true;
+            // player.transform.rotation = Quaternion.LookRotation(player.transform.position);
         }
         if (Input.GetKey(KeyCode.D)) {
-            player.transform.position += player.transform.localRotation * new Vector3(-speed, 0, 0);
-            // player.transform.rotation *= Quaternion.AngleAxis(speed * 150, Vector3.up);
+            // Setze Blickrichtung des Players auf Blickrichtung der Kamera
+            player.transform.forward = -GameObject.Find("Main Camera").transform.right;
+            // da Kamera nicht exakt geradeaus schaut, muss Player-Position nachjustiert werden
+            // player.transform.Rotate(23.66f, player.transform.position.y, 0);
+            player.transform.position += player.transform.localRotation * new Vector3(0, 0, -speed);
+            // player.transform.rotation = Quaternion.LookRotation(player.transform.position);
         }
     }
-     
+
+    void freeLookCamera() {
+        float mouseSpeed = 2.0f;
+       
+       // Drehung um y-Achse (Vector3.up) in Relation zur Drehbewegung der Maus -> Input.GetAxis("Mouse X")
+       // mit mouseSpeed multiplizieren, da Bewegung sonst sehr langsam ist
+       // Rotation (Quaternion.AngleAxis()) mit cameraView multiplizieren, sodass cameraView neue Position der Kamera speichert
+        cameraView = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * mouseSpeed, Vector3.up) * cameraView;
+        // addiere Kameraposition mit cameraView, damit Kamera immer gleichen Abstand zum Player aufweist
+        Camera.main.transform.position = player.transform.position + cameraView;
+        // Kamera sieht immer zum Player
+        Camera.main.transform.LookAt(player.transform.position);
+        // Verändere Rotation der Kamera, sodass eine bessere Kamerasicht entsteht -> ansonsten zu viel Fokus auf Player durch Transform.LookAt(target)
+        Camera.main.transform.Rotate(-15.0f, 0, 0);
+    }
 }
