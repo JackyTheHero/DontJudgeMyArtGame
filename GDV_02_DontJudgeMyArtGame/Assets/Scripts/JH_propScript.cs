3db6e3b27209bdf08f5c02c68a719dc688546114
JackyTheHero <held.jacqueline@gmx.de>
2020-07-23 20:37:33 +0200

    placed benches, new statue Mesh WIP
    placed all eleven bench meshes
    added new statue Mesh to script, statue foundation mesh done

--- GDV_02_DontJudgeMyArtGame/Assets/Scripts/JH_propScript.cs
+++ GDV_02_DontJudgeMyArtGame/Assets/Scripts/JH_propScript.cs
@@ -10,12 +10,34 @@ public class JH_propScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Standard Bench
         GameObject bench = new GameObject("bench", typeof(MeshFilter), typeof(MeshRenderer));
         Renderer benchRend = bench.GetComponent<Renderer>();
         benchRend.material = new Material(Shader.Find("Standard"));
-        Texture texture = Resources.Load("TextureGreen") as Texture;
+        Texture texture = Resources.Load("holz") as Texture;
         benchRend.material.mainTexture = texture;
         bench = createBench(bench);
+
+        MeshCollider doorCollider = bench.AddComponent<MeshCollider>();
+
+        //Bench Parent
+        GameObject benchParent = new GameObject("benchParent");
+
+        //Bench placing
+        placingBenches(bench, benchParent);
+
+        bench.transform.Translate(-20,30,20);
+
+        GameObject statue = new GameObject("statue", typeof(MeshFilter), typeof(MeshRenderer));
+        Renderer statueRend = statue.GetComponent<Renderer>();
+        benchRend.material = new Material(Shader.Find("Standard"));
+        Texture statueTexture = Resources.Load("TexturePurple") as Texture;
+        statueRend.material.mainTexture = texture;
+        statue = createStatue(statue);
+
+        MeshCollider statueCollider = statue.AddComponent<MeshCollider>();
+
+
     }
 
     public GameObject createBench(GameObject benchMaster) {
@@ -279,6 +301,198 @@ public class JH_propScript : MonoBehaviour
         
 
         return benchMaster;
+    }
+
+    public void placingBenches(GameObject bench, GameObject parent) {
+
+        GameObject benchOne = Instantiate(bench);
+        benchOne.name = "benchOne";
+        benchOne.transform.Translate(17.5f, 0f, -28f);
+        benchOne.transform.Rotate(new Vector3(0,90,0));
+
+        GameObject benchTwo = Instantiate(bench);
+        benchTwo.name = "benchTwo";
+        benchTwo.transform.Translate(16.5f, 0f, 26f);
+        benchTwo.transform.Rotate(new Vector3(0,90,0));
+
+        GameObject benchThree = Instantiate(bench);
+        benchThree.name = "benchThree";
+        benchThree.transform.Translate(83f, 0f, 11.5f);
+
+        GameObject benchFour = Instantiate(bench);
+        benchFour.name = "benchFour";
+        benchFour.transform.Translate(155f, 0f, 11.5f);
+
+        GameObject benchFive = Instantiate(bench);
+        benchFive.name = "benchFive";
+        benchFive.transform.Translate(158f, 0f, -43.5f);
+
+        GameObject benchSix = Instantiate(bench);
+        benchSix.name = "benchSix";
+        benchSix.transform.Translate(88f, 0f, -85.5f);
+
+        GameObject benchSeven = Instantiate(bench);
+        benchSeven.name = "benchSeven";
+        benchSeven.transform.Translate(158f, 0f, -19f);
+
+        GameObject benchEight = Instantiate(bench);
+        benchEight.name = "benchEight";
+        benchEight.transform.Translate(158f, 0f, -13f);
+
+        GameObject benchNine = Instantiate(bench);
+        benchNine.name = "benchNine";
+        benchNine.transform.Translate(30f, 0f, -75f);
+        benchNine.transform.Rotate(new Vector3(0, 90, 0));
+
+        GameObject benchTen = Instantiate(bench);
+        benchTen.name = "benchTen";
+        benchTen.transform.Translate(36f, 0f, -75f);
+        benchTen.transform.Rotate(new Vector3(0, 90, 0));
+
+        GameObject benchEleven = Instantiate(bench);
+        benchEleven.name = "benchEleven";
+        benchEleven.transform.Translate(131f, 0f, 11.5f);
+
+        benchOne.transform.parent = parent.transform;
+        benchTwo.transform.parent = parent.transform;
+        benchThree.transform.parent = parent.transform;
+        benchFour.transform.parent = parent.transform;
+        benchFive.transform.parent = parent.transform;
+        benchSix.transform.parent = parent.transform;
+        benchSeven.transform.parent = parent.transform;
+        benchEight.transform.parent = parent.transform;
+        benchNine.transform.parent = parent.transform;
+        benchTen.transform.parent = parent.transform;
+        benchEleven.transform.parent = parent.transform;
+    }
+
+    public GameObject createStatue(GameObject statueMaster) {
+        Mesh statueMesh = new Mesh();
+        GameObject foundation = new GameObject("foundation", typeof(MeshFilter), typeof(MeshRenderer));
+
+        Renderer foundationRend = foundation.GetComponent<Renderer>();
+        foundationRend.material = new Material(Shader.Find("Standard"));
+
+        statueMesh = foundation.GetComponent<MeshFilter>().mesh;
+
+        statueMesh.Clear();
+
+        List<Vector3> statueVertices = new List<Vector3>();
+        List<int> statueTriangles = new List<int>();
+        List<Vector2> statueUv = new List<Vector2>();
+
+        //foundation 1
+        statueVertices.Add(new Vector3(-7, 0, 7));
+        statueVertices.Add(new Vector3(7, 0, 7));
+        statueVertices.Add(new Vector3(7, 2, 7));
+        statueVertices.Add(new Vector3(-7, 2, 7));
+
+        //foundation 2
+        statueVertices.Add(new Vector3(7, 0, 7));
+        statueVertices.Add(new Vector3(7, 0, -7));
+        statueVertices.Add(new Vector3(7, 2, -7));
+        statueVertices.Add(new Vector3(7, 2, 7));
+
+        //foundation 3
+        statueVertices.Add(new Vector3(7, 0, -7));
+        statueVertices.Add(new Vector3(-7, 0, -7));
+        statueVertices.Add(new Vector3(-7, 2, -7));
+        statueVertices.Add(new Vector3(7, 2, -7));
+
+        //foundation 4
+        statueVertices.Add(new Vector3(-7, 0, -7));
+        statueVertices.Add(new Vector3(-7, 0, 7));
+        statueVertices.Add(new Vector3(-7, 2, 7));
+        statueVertices.Add(new Vector3(-7, 2, -7));
+
+        //foundation 5
+        statueVertices.Add(new Vector3(-7, 2, 7));
+        statueVertices.Add(new Vector3(7, 2, 7));
+        statueVertices.Add(new Vector3(7, 2, -7));
+        statueVertices.Add(new Vector3(-7, 2, -7));
+
+
+        statueMesh.vertices = statueVertices.ToArray();
+
+        //foundation 1
+        statueTriangles.Add(0);
+        statueTriangles.Add(1);
+        statueTriangles.Add(2);
+        statueTriangles.Add(0);
+        statueTriangles.Add(2);
+        statueTriangles.Add(3);
+
+        //foundation 2
+        statueTriangles.Add(4);
+        statueTriangles.Add(5);
+        statueTriangles.Add(6);
+        statueTriangles.Add(4);
+        statueTriangles.Add(6);
+        statueTriangles.Add(7);
+
+        //foundation 3
+        statueTriangles.Add(8);
+        statueTriangles.Add(9);
+        statueTriangles.Add(10);
+        statueTriangles.Add(8);
+        statueTriangles.Add(10);
+        statueTriangles.Add(11);
+
+        //foundation 4
+        statueTriangles.Add(12);
+        statueTriangles.Add(13);
+        statueTriangles.Add(14);
+        statueTriangles.Add(12);
+        statueTriangles.Add(14);
+        statueTriangles.Add(15);
+
+        //foundation 5
+        statueTriangles.Add(16);
+        statueTriangles.Add(17);
+        statueTriangles.Add(18);
+        statueTriangles.Add(16);
+        statueTriangles.Add(18);
+        statueTriangles.Add(19);
+
+        statueMesh.triangles = statueTriangles.ToArray();
+
+        //foundation 1
+        statueUv.Add(new Vector2(0, 0));
+        statueUv.Add(new Vector2(1, 0));
+        statueUv.Add(new Vector2(1, 1));
+        statueUv.Add(new Vector2(0, 1));
+
+        //foundation 2
+        statueUv.Add(new Vector2(0, 0));
+        statueUv.Add(new Vector2(1, 0));
+        statueUv.Add(new Vector2(1, 1));
+        statueUv.Add(new Vector2(0, 1));
+
+        //foundation 3
+        statueUv.Add(new Vector2(0, 0));
+        statueUv.Add(new Vector2(1, 0));
+        statueUv.Add(new Vector2(1, 1));
+        statueUv.Add(new Vector2(0, 1));
+
+        //foundation 4
+        statueUv.Add(new Vector2(0, 0));
+        statueUv.Add(new Vector2(1, 0));
+        statueUv.Add(new Vector2(1, 1));
+        statueUv.Add(new Vector2(0, 1));
+
+        //foundation 5
+        statueUv.Add(new Vector2(0, 0));
+        statueUv.Add(new Vector2(1, 0));
+        statueUv.Add(new Vector2(1, 1));
+        statueUv.Add(new Vector2(0, 1));
+
+        statueMesh.uv = statueUv.ToArray();
+
+        statueMesh.RecalculateNormals();
+
+        foundation.transform.parent = statueMaster.transform;
+
+        return statueMaster;
     }
 
     public void CombineMesh(GameObject obj) {
