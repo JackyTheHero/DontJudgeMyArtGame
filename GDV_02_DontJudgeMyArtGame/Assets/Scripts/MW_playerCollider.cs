ffdbfa66d9a03defdcd4e346d8b7a7f83652dfba
Melanie Werner <64008638+Anlariel@users.noreply.github.com>
2020-07-28 12:43:20 +0200

    First range detection of an object
    - added second collider to the player but this time it's a trigger collider in order to use OnTriggerEnter and OnTriggerExit
    - added OnTriggerEnter and OnTriggerExit event in order to check if a gameObject is close by
    - only working for "Fenster (1)" right now since I have just tried to find a way to make something like that work
    - OnGUI event is telling the player if he can delete the object

--- GDV_02_DontJudgeMyArtGame/Assets/Scripts/MW_playerCollider.cs
+++ GDV_02_DontJudgeMyArtGame/Assets/Scripts/MW_playerCollider.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class MW_playerCollider : MonoBehaviour
 {
+    // speichere in dieser Variablen, ob ein interagierbares Gameobject in der Nähe ist
+    bool isInRange = false;
+
+    // speichere das GameObject, das ein OnTriggerEnter mit dem Player hat
+    GameObject inFocus;
 
     // Keyboard wird bei Start der Szene gesperrt, da Player aus einer gewissen Höhe auf den Boden fällt ...
     // ... während dieses Falles soll sich der Player nicht bewegen können
@@ -17,7 +22,10 @@ public class MW_playerCollider : MonoBehaviour
 
     // Update is called once per frame
     void Update () {
-      
+        if (isInRange == true && Input.GetKeyDown(KeyCode.E)) {
+            Destroy(inFocus);
+            isInRange = false;
+        }
     }
 
     void OnCollisionEnter(Collision other) {
@@ -27,6 +35,37 @@ public class MW_playerCollider : MonoBehaviour
         // wenn Player mit dem Boden kollidiert und somit der Fall beendet ist, wird Tasteneingabe freigegeben
         if (other.gameObject.name == "Ground") {
             keyboardEnabled = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.name == "Fenster (1)") {
+            Debug.Log(this.name + "is able to interact with" + other.gameObject.name);
+            isInRange = true;
+            inFocus = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.gameObject.name == "Fenster (1)") {
+            Debug.Log(this.name + "is no longer able to interact with" + other.gameObject.name);
+            isInRange = false;
+        }
+    }
+
+// Text erscheint oben links in der Ecke
+    // mit Variable collectedItems werden die eingesammelten Items gezählt
+    void OnGUI() {
+        GUIStyle style = new GUIStyle();
+        style.fontSize = 24;
+        // Färbe Schrift rot, um die besser lesen zu können
+        style.normal.textColor = Color.red;
+        // collectedItems wird in triggerEnter (Skript am Schlangenkopf) deklariert und hochgezählt
+        if (isInRange == true) {
+            GUI.Label(new Rect(10, 0, 0, 0), "Drücke E, um das Fenster zu löschen", style);
+        }
+        if (isInRange == false) {
+            GUI.Label(new Rect(10, 0, 0, 0), "", style);
         }
     }
 }
