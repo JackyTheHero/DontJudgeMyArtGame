b551d69b1f67f253c3169d4b12f3f0d8855c5c97
JackyTheHero <held.jacqueline@gmx.de>
2020-08-27 19:18:35 +0200

    Visitor Behaviour and scoring system
    - deleted unnecessary script (JackyScript.cs)
    - deleted unnecessary object (visitorScriptTest)
    - moved visitor to a place near the first owned painting
    - added visitorBehaviour.cs to visitor figure
    - visitorBehaviour.cs: visitor reacts with a speech bubble if near a painting and raises/lowers the general score
    - scoreMaster.cs: controls all scores (general / paintings); currently only general score is active

---
+++ GDV_02_DontJudgeMyArtGame/Assets/Scripts/JH_scoreMaster.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//instance of JH_scoreMaster in JH_visitorBehaviour.cs
+
+public class JH_scoreMaster : MonoBehaviour
+{
+    public int generalScore;
+    public int[] paintingScore;
+    public Boolean gameover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        generalScore = 0;
+        gameover = false;
+        //here initializing paintingScore through the method Gameobject.FindGameObjectsWithTag("ownedPainting")
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (generalScore == -10) {
+            gameover = true;
+            Debug.Log("GAME OVER! You reached a general score of -10!");
+        }
+
+        if (Input.GetKeyDown("c"))
+        {
+            Debug.Log(generalScore);
+        }
+    }
+
+    public void raiseScore(GameObject painting) {
+        generalScore += 3;
+        //paintingScore on the right index raised (read the name of the painting or add a painting array with the gameobjects to transfer right index)
+    }
+
+    public void lowerScore(GameObject painting) {
+        generalScore -= 2;
+        //paintingScore on the right index lowered (read the name of the painting or add a painting array with the gameobjects to transfer right index)
+    }
+}
---
+++ GDV_02_DontJudgeMyArtGame/Assets/Scripts/JH_visitorBehaviour.cs
@@ -0,0 +1,97 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* TO-DO:
+- visitor walks around
+- if near a painting by the player: walks to it and activates speech bubble   || in progress; visitor reacts with speechbubble if Trigger is entered
+- mostly random review by speech bubble: positive or negative
+- review will be added to picture score and general score
+- visitor walks away --> loops begins again
+
+ - visitors become invisible when player is in interaction menu
+ */
+
+
+public class JH_visitorBehaviour : MonoBehaviour
+{
+
+    // public GameObject visitor; // visitor height --> 4 tiles
+    public GameObject textBubble;
+    public Boolean nearPainting;
+    private Rigidbody visitRigid;
+    private int goodOrBad;
+    public JH_scoreMaster scoreMaster;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set textBubble to visitorPosition and set parent to visitor figure
+        textBubble = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        textBubble.SetActive(false);
+
+        textBubble.transform.position = this.transform.position;
+        textBubble.transform.parent = this.transform;
+
+        nearPainting = false;
+
+        //adding Collider and Rigidbody for Trigger recognition
+        Collider visitorColl = this.GetComponent<Collider>();
+
+        visitRigid = this.GetComponent<Rigidbody>();
+        visitRigid.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
+        scoreMaster = this.gameObject.AddComponent<JH_scoreMaster>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nearPainting) {
+            speak();
+        }
+    }
+
+    public void speak()
+    {
+        
+        //constraints to freeze the figure in place
+        visitRigid.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
+        //need something like Time.time to time the review (maybe 10 secs active)
+
+        //move up speechbubble until it reaches its position at y = 5.0f
+        textBubble.SetActive(true);
+        if (textBubble.transform.position.y < 5.0f) {
+            textBubble.transform.position += new Vector3(0.0f, 0.02f, 0.0f);
+        } else {
+            textBubble.transform.Rotate(0.0f, 0.1f, 0.0f); 
+        }
+    }
+
+    
+    void OnTriggerEnter(Collider other) {
+        // gib folgende Zeile für alle Kollisionen aus
+       Debug.Log(this.name + " has an OnTriggerEnter with " + other.gameObject.name);
+       
+        //if trigger of owned painting was entered
+        if (other.gameObject.name.Contains("owned")) {
+            nearPainting = true;
+
+            //random number decides if there is a good or bad review (0 == bad, 1 == good)
+            goodOrBad = (int)UnityEngine.Random.Range(0f, 2f);
+
+            if (goodOrBad == 1)
+            {
+                scoreMaster.raiseScore(this.gameObject);
+            }
+            else {
+                scoreMaster.lowerScore(this.gameObject);
+            }
+        }
+    }
+
+
+}
