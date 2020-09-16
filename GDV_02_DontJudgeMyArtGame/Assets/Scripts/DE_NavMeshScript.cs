3ae6083891c4c78ea6edbea7e1099ac435d7d8a6
Daiuuus <63926891+Daiuuus@users.noreply.github.com>
2020-09-16 21:18:41 +0200

    NavMesh stuff

---
+++ GDV_02_DontJudgeMyArtGame/Assets/Scripts/DE_NavMeshScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DE_NavMeshScript : MonoBehaviour
+{
+    
+    public NavMeshSurface surface;
+
+    void Start()
+    {
+       surface.BuildNavMesh();
+        
+    }
+
+}
