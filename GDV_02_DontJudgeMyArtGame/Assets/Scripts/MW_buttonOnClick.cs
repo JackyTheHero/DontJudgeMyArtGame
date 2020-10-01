7bd6c4ab649784ee6d070c832d7d8675861a452c
Melanie Werner <64008638+Anlariel@users.noreply.github.com>
2020-10-01 20:27:17 +0200

    Script comments

--- GDV_02_DontJudgeMyArtGame/Assets/Scripts/MW_buttonOnClick.cs
+++ GDV_02_DontJudgeMyArtGame/Assets/Scripts/MW_buttonOnClick.cs
@@ -7,10 +7,13 @@ public class MW_buttonOnClick : MonoBehaviour
     public GameObject canvas;
     
     public void ContinueGame() {
+        // Menü wird geschlossen, wenn man auf Button "Fortsetzen" ...
+        // ... oder in unserem Fall ebenso "Neues Spiel" klickt
         canvas.gameObject.SetActive(false);
     }
 
     public void QuitGame() {
+        // Spiel wird geschlossen, wenn man auf Button "Ende" klickt
         Application.Quit();
     }
 }
--- GDV_02_DontJudgeMyArtGame/Assets/Scripts/MW_mainMenu.cs
+++ GDV_02_DontJudgeMyArtGame/Assets/Scripts/MW_mainMenu.cs
@@ -27,49 +27,66 @@ public class MW_mainMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mouseCursor();
+        
+        // Funktion, die Mouse-Cursor im Menü ein- und im Spiel ausblendet ...
+        // ... und zusätzlich das Spiel pausiert, wenn das Menü geöffnet ist
+        pauseGame();
 
+        // Funktion, die checkt, ob man sich gerade im Menü befindet oder nicht ...
+        // ... zusätzliche Prüfung, ob das Spiel schon gestartet wurde
         checkMenu();
 
+        // Funktion, die das Menü aufruft oder schließt
         startMenu();
     }
 
+    // Funktion, die checkt, ob man sich gerade im Menü befindet oder nicht ...
+    // ... zusätzliche Prüfung, ob das Spiel schon gestartet wurde
     void checkMenu() {
         if (canvas.gameObject.activeSelf == true) {
+            // canvas gerade aktiv -> dann setze isInMenu auf true
             isInMenu = true;
         } else {
+            // ansonsten auf false
             isInMenu = false;
         }
 
         // playerPos ist nach direktem Start nicht 5, sondern liegt bei ca. 4.99f
         if (player.transform.position.y >= playerPos - 0.01f) {
+            // wenn Anfangsposition nicht abweicht, wurde Spiel noch nicht gestartet -> "Neues Spiel"
             continueGame.gameObject.SetActive(false);
             newGame.gameObject.SetActive(true);
         } else {
+            // wenn Anfangsposition abweicht, wurde Spiel schon gestartet -> "Fortsetzen"
             continueGame.gameObject.SetActive(true);
             newGame.gameObject.SetActive(false);
         }
     }
 
+    // Funktion, die das Menü aufruft oder schließt
     void startMenu() {
         if (Input.GetKeyDown(KeyCode.Escape)) {
             if (isInMenu == false) {
+                // wenn Menü geschlossen ist, wird es mit der Escape-Taste geöffnet
                 canvas.gameObject.SetActive(true);
             }
             if (isInMenu == true) {
+                // wenn Menü offen ist, wird es mit der Escape-Taste geschlossen
                 canvas.gameObject.SetActive(false);
             }
         }
     }
 
-    void mouseCursor() {
+    // Funktion, die Mouse-Cursor im Menü ein- und im Spiel ausblendet ...
+    // ... und zusätzlich das Spiel pausiert, wenn das Menü geöffnet ist
+    void pauseGame() {
         // Maus-Cursor wird ausgeblendet, wenn man sich nicht im Menü befindet
         if (isInMenu == true) {
-            Cursor.visible = true;
-            Time.timeScale = 0;
+            Time.timeScale = 0; // Spiel pausiert
+            Cursor.visible = true; // Cursor sichtbar   
         } else {
-            Cursor.visible = false;
-            Time.timeScale = 1;
+            Cursor.visible = false; // Cursor unsichtbar
+            Time.timeScale = 1; // Spiel geht weiter
         }
     }
 }
